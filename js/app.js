f8a333b2d7d9a491e8bd0cdd89b1fca38704ff8c
caraujo07 <caraujo@jc.com.br>
2018-09-11 15:59:52 -0300

    Adicionada a função para análise de paricidade do número

--- js/app.js
+++ js/app.js
@@ -40,6 +40,7 @@ function tenpc() {
 
 // X% of a number
 function xpc() {
+
     var number = document.getElementById("campo1").value;
     var pct = document.getElementById("campo2").value * 0.01;
     var total = (number * pct);
@@ -47,13 +48,27 @@ function xpc() {
     event.preventDefault();
 
     if(campo1.value !== 0 && campo2.value !== 0) {
-       
         document.getElementById("resposta").innerHTML = arredondado;
     }else{
         document.getElementById("resposta").innerHTML = "Digite apenas números";
     } 
+}
 
 
+function imparoupar() {
+    var number = document.getElementById("campo3").value;
+    var res = (number%2);
+    event.preventDefault();
+ 
+    if(number == 0){
+        document.getElementById("resposta").innerHTML = "O número digitado foi o zero!";
+    }else if(res === 0) {
+        document.getElementById("resposta").innerHTML = "O número digitado é par!";
+    }else if(res === 1) {
+        document.getElementById("resposta").innerHTML = "O número digitado é ímpar!";
+    }else {
+        document.getElementById("resposta").innerHTML = "O caractere digitado é inválido!";
+    }
 }
 
 
